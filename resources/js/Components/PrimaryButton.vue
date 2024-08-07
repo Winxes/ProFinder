3c6c5afb2b85f6cb962f3cf8b69bb7f98357d4c3
marantonieto <marcioantoni16@gmail.com>
2024-08-07 05:35:27 -0300

    login created

---
+++ resources/js/Components/PrimaryButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'submit',
+    },
+});
+</script>
+
+<template>
+    <button :type="type" class="inline-flex items-center justify-center bg-[#939CCE] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+        <slot />
+    </button>
+</template>
