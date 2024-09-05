9ab2e125afa019c37aff70ce389cf06d4fd4b0a9
marcioantfilho <marcioantoni16@gmail.com>
2024-09-05 17:52:55 -0300

    FEAT-responsive-register-and-login

---
+++ resources/js/Components/AuthenticationCardReg.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen flex flex-col sm:flex-row sm:justify-center items-center pt-0 sm:pt-6 bg-gray-100">
+        <!-- Container para o logo -->
+        <div class="w-full sm:w-[500px] mt-6 px-6 py-4 bg-[#7C93C3] shadow-md rounded-lg flex flex-col items-center hidden sm:block">
+            <slot name="logo" />
+            <img :src="'images/login-woman.png'" alt="Company Logo" class="max-w-full h-auto" />
+        </div>
+        
+        <!-- Container para o conteúdo principal -->
+        <div class="w-full sm:w-[500px] mt-0 sm:mt-6 px-6 py-4 bg-white shadow-md rounded-lg flex flex-col items-center h-screen sm:h-[610px]">
+            <slot />
+        </div>
+    </div>
+</template>
