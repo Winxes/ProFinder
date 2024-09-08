e9292ed0e8d6de7df0e8ecede30e317193ad678b
marcelahadassa <marcelahadassa.pereira@gmail.com>
2024-09-08 18:22:44 -0300

    feat: box do post componentizado

--- resources/js/Components/SearchInput.vue
+++ resources/js/Components/SearchInput.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { PhMagnifyingGlass } from '@phosphor-icons/vue';
 
 
 </script>
@@ -15,25 +14,9 @@ import { PhMagnifyingGlass } from '@phosphor-icons/vue';
             d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
         </svg>
       </div>
-      <input type="search" id="default-search" class="h-2 md:w-[442px] rounded-full
-     bg-gray-100 p-4 ps-10 text-sm focus:border-blue-500 focus:ring-blue-500
-      dark:border-gray-600 dark:text-black dark:placeholder-gray-400
-       dark:focus:border-blue-500 dark:focus:ring-primaria" 
-       placeholder="Pesquisar" 
-       required />
+      <input type="search" id="default-search"
+        class="h-2 md:w-[442px] rounded-full bg-gray-100 p-4 ps-10 text-sm focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:text-black dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-primaria"
+        placeholder="Pesquisar" required />
     </div>
   </form>
 </template>
-
-
-
-<!-- <div className="relative px-4 sm:px-8 md:px-12 lg:px-24 flex justify-between">
-    <PhMagnifyingGlass className="size-5"></PhMagnifyingGlass>
-    <input class = "ml-auto mb-2 my-3 rounded-xl 
-    w-full md:w-[442px] focus:outline-none 
-    focus:ring-2 focus:ring-primaria" 
-    type="text" 
-    icon={PhMagnifyingGlass}
-    placeholder="Pesquisar">
-</input>
-</div> -->
