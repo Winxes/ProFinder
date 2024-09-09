4eb459199d54fd4a773e0d51824004a0367dca74
marcioantfilho <marcioantoni16@gmail.com>
2024-09-09 19:57:04 -0300

    FEAT-Scratch-landing-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'lightblue': '#808AC5',
+            },
         },
     },
 
