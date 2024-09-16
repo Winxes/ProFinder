9bf5d05527067a240852a3599a268518480c4be8
marcelahadassa <marcelahadassa.pereira@gmail.com>
2024-09-16 17:27:02 -0300

    feat: dashboard filters modal
    Co-authored-by: LucasAurelio <LucasAurelio0@users.noreply.github.com>

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -21,6 +21,7 @@ const submit = () => {
 </script>
 
 <template>
+
     <Head title="Forgot Password" />
 
     <AuthenticationCard>
@@ -29,7 +30,7 @@ const submit = () => {
         </template>
 
         <div class="pt-60 mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+            Esqueceu a senha? Digite seu e-mail usado na hora do cadastro.
         </div>
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -39,21 +40,14 @@ const submit = () => {
         <form @submit.prevent="submit">
             <div>
                 <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                <TextInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required autofocus
+                    autocomplete="username" />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
+                    Enviar link de redefinição de senha
                 </PrimaryButton>
             </div>
         </form>
